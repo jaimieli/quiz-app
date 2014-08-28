37e9ada338ae87e44c5c5ab74be812b6836d6171
Jaimie Li <jaimieli@gmail.com>
2014-08-28 17:23:21 -0400

    added score and submitting question functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,26 +17,47 @@ angular.module('quizApp')
     $scope.quiz = [
       {
         "q": "Who is the best ping pong player at FSA?",
-        'options': [{ 'value': "Mike"} , { 'value': "Eddie"} , {answer : "Nimit"} , { 'value': "Patrick"}],
+        'options': [{ 'value': "Mike"} , { 'value': "Eddie"} , {'value' : "Nimit"} , { 'value': "Patrick"}],
         'answer': "Nimit",
         'difficulty': 3
       },
       { "q": "Which robot name was chanted during Lego Mindstorms?",
-        'options':[{ 'value': 'infiniteLoop'} , { 'value': 'noHope.js'} , {answer : 'johnny5'} , { 'value': 'none of the above'}],
+        'options':[{ 'value': 'infiniteLoop'} , { 'value': 'noHope.js'} , {'value' : 'johnny5'} , { 'value': 'none of the above'}],
         'answer':'noHope.js',
-        'difficulty': 2
-      },
-      {
-        'q': "Out of the following frontend frameworks, which framework is most rails-like",
-        'options':[{ 'value': 'Ember.js'} ,{ 'value': 'Angular.js'} , {answer : 'Backbone.js'} , { 'value': 'Meteor.js'}],
-        'answer':'Ember.js',
         'difficulty': 1
       },
       {
+        'q': "Out of the following frontend frameworks, which framework is most rails-like",
+        'options':[{ 'value': 'Ember.js'} ,{ 'value': 'Angular.js'} , {'value' : 'Backbone.js'} , { 'value': 'Meteor.js'}],
+        'answer':'Ember.js',
+        'difficulty': 2
+      },
+      {
         'q': "Which project used a local data store?",
-        'options':[{ 'value': 'TripPlanner'} ,{ 'value': 'Twitter.js'} , {answer : 'WikiWalker'} , { 'value': 'WikiStack'}],
+        'options':[{ 'value': 'TripPlanner'} ,{ 'value': 'Twitter.js'} , {'value' : 'WikiWalker'} , { 'value': 'WikiStack'}],
         'answer':'Twitter.js',
         'difficulty': 3
       }
     ];
+    // $scope.letters = ['A', 'B', 'C', 'D'];
+    $scope.check = function(opt, quest) {
+      if (opt===quest){
+        $scope.score ++;
+      }
+    };
+    $scope.score = 0;
+    $scope.submit = function() {
+      $scope.quiz.push({
+        'q':this.question,
+        'options': [
+          {'value': this.option1},
+          {'value': this.option2},
+          {'value': this.option3},
+          {'value': this.option4}
+          ],
+        'answer': this.answer,
+        'difficulty': +this.difficulty
+      })
+      $
+    }
   });

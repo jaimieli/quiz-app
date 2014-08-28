5eb4043802a6b693742843d7641fd97eab0c2a7f
Jaimie Li <jaimieli@gmail.com>
2014-08-28 15:25:52 -0400

    formatted questions to show answer options, order by difficulty, and display question number

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,21 +18,25 @@ angular.module('quizApp')
       {
         "q": "Who is the best ping pong player at FSA?",
         'options': [{ 'value': "Mike"} , { 'value': "Eddie"} , {answer : "Nimit"} , { 'value': "Patrick"}],
-        'answer': "Nimit"
+        'answer': "Nimit",
+        'difficulty': 3
       },
       { "q": "Which robot name was chanted during Lego Mindstorms?",
         'options':[{ 'value': 'infiniteLoop'} , { 'value': 'noHope.js'} , {answer : 'johnny5'} , { 'value': 'none of the above'}],
-        'answer':'noHope.js'
+        'answer':'noHope.js',
+        'difficulty': 2
       },
       {
         'q': "Out of the following frontend frameworks, which framework is most rails-like",
         'options':[{ 'value': 'Ember.js'} ,{ 'value': 'Angular.js'} , {answer : 'Backbone.js'} , { 'value': 'Meteor.js'}],
-        'answer':'Ember.js'
+        'answer':'Ember.js',
+        'difficulty': 1
       },
       {
         'q': "Which project used a local data store?",
         'options':[{ 'value': 'TripPlanner'} ,{ 'value': 'Twitter.js'} , {answer : 'WikiWalker'} , { 'value': 'WikiStack'}],
-        'answer':'Twitter.js'
+        'answer':'Twitter.js',
+        'difficulty': 3
       }
     ];
   });

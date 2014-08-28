86e666d9ceaed34268825359a8ce6442165e6681
Jaimie Li <jaimieli@gmail.com>
2014-08-28 15:01:20 -0400

    initialized app

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name quizApp
+ * @description
+ * # quizApp
+ *
+ * Main module of the application.
+ */
+
+// ngCookies, ngResource, and ngRoute are dependencies
+angular
+  .module('quizApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });

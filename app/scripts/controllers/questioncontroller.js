8279247340d604bb2e7b764af2d68704f8bb2a4b
Jaimie Li <jaimieli@gmail.com>
2014-09-02 12:32:55 -0400

    validating option length, storing answer, and adding to question view

--- app/scripts/controllers/questioncontroller.js
+++ app/scripts/controllers/questioncontroller.js
@@ -11,12 +11,20 @@ angular.module('quizApp')
   .controller('QuestionController', function ($scope) {
     this.newQuestion = {options:[]};
     this.options=[];
+    this.count = 0;
     this.addOption = function() {
       this.newQuestion.options.push({value:''});
+      this.count++;
     };
     this.submit = function() {
-      this.newQuestion.difficulty = +this.newQuestion.difficulty;
-      $scope.quiz.push(this.newQuestion);
-      this.newQuestion = {options:[]};
+      if(this.count < 2) {
+        return alert('You need at least 2 options.');
+      }
+      else {
+        this.newQuestion.difficulty = +this.newQuestion.difficulty;
+        $scope.quiz.push(this.newQuestion);
+        this.newQuestion = {options:[]};
+        this.count = 0;
+      }
     };
   });
